8d694b1c39f92749be72b7f7cc503e8c0374ec4d
Nirmal201 <ninu201@gmail.com>
2020-05-16 05:58:15 -0400

    cors error fixed

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 morgan.token("justBaoddy", function (req, res) {
   return JSON.stringify(req.body);
