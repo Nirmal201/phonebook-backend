5761720a8ce9085dd0c88ca362d7c0495e595bd7
Nirmal201 <ninu201@gmail.com>
2020-05-16 20:00:14 -0400

    adding frontend

---
+++ build/precache-manifest.50e55db4d4ea16f667bedb8b33741ec4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41de89f6b6c1d3409903844551e858c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f20e0b1ace3ae8157754",
+    "url": "/static/css/main.82f0e322.chunk.css"
+  },
+  {
+    "revision": "8e6fc279ec740abde24c",
+    "url": "/static/js/2.6983b81b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6983b81b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f20e0b1ace3ae8157754",
+    "url": "/static/js/main.d782ae70.chunk.js"
+  },
+  {
+    "revision": "bccee6d650832870a352",
+    "url": "/static/js/runtime-main.5401d62a.js"
+  }
+]);
